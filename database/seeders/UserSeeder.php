52ca49b4bab314249384b01dfa2be8eaae9fc2d7
larasajaa <sayalaraswati@gmail.com>
2024-03-02 12:27:30 +0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123'),
+            'role' => 'admin'
+        ]);
+        User::create([
+            'name' => 'petugas',
+            'email' => 'petugas@gmail.com',
+            'password' => bcrypt('123'),
+            'role' => 'petugas'
+        ]);
+    }
+}
